7a5a0e6619de3321508f77d2453a3a172e63f021
DusanStevic <stevicdule@gmail.com>
2021-09-14 11:40:17 +0200

    bugfix post#10

--- publish-dockerhub.sh
+++ publish-dockerhub.sh
@@ -5,7 +5,7 @@ DOCKERHUB_USERNAME=${2}
 DOCKERHUB_PASSWORD=${3}
 
 
-POST=${DOCKERHUB_USERNAME}/nistagram-user-service:${VERSION}
+POST=${DOCKERHUB_USERNAME}/nistagram-post-service:${VERSION}
 
 DOCKER_BUILDKIT=1 docker build -t ${POST} --no-cache .
 
